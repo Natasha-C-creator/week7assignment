35381e71acfbc6d2da8b4397bc6489485959f62a
Natasha C <n.coward@hotmail.com>
2025-01-31 10:55:41 +0000

    Added Browser Router and set up Route path to comments section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Form from "./components/Form.jsx";
@@ -8,10 +9,14 @@ import Footer from "./components/Footer.jsx";
 export default function App() {
   return (
     <>
-      <Header />
-      <Form />
-      <Comments />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Form />
+        <Routes>
+          <Route path="/components/:comments" element={<Comments />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
