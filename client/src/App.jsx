26e374d8d034efbd598974249d06e567b78645c6
Natasha C <n.coward@hotmail.com>
2025-01-31 10:40:16 +0000

    Comments section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Form from "./components/Form.jsx";
+import Comments from "./components/Comments.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <>
       <Header />
       <Form />
+      <Comments />
       <Footer />
     </>
   );
