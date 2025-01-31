f6cf2f8354ba5afe0ff52cf71eccb69812dc8615
Natasha C <n.coward@hotmail.com>
2025-01-31 10:22:27 +0000

    Header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   );
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <h2 className="header">Wedding Guest Comments ❤️ </h2>
+    </>
+  );
+}
